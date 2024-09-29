c1008a592cf238313e4d814cd872bb1ca82d0d84
YeakubDurjoy001 <yeakubdurjoy002>
2024-09-29 08:59:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Header from "./components/header/header";
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h1>Memorable Bottles</h1>
+    </div>
+  );
+}
+
+export default Header;
